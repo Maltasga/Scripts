c20cfa9d2ecafb701a1cc9744586587e3b9041db
Gabriel Malta <gmalta44@gmail.com>
2017-03-07 19:17:32 -0300

    novas funcionalidades

--- MaltaLib.js
+++ MaltaLib.js
@@ -1,21 +1,63 @@
 (function () {
+    "use strict";
+    window.maltalib = window.maltalib | {};
+
     function _formataData(data) {
-        if (!data.getDate) throw new Error("A data informada deve ser um objeto do tipo Date");
-        var dia = (function (d) { if (d < 9) return "0" + d; return "" + d; })(data.getDate());
-        var mes = (function (m) { m += 1; if (m < 9) return "0" + m; return "" + m; })(data.getMonth());
-        var ano = data.getFullYear();
+        var dia = "", mes = "", ano = 0;
+        if (!_isTypeof("date", data)) throw new Error("A data informada deve ser um objeto do tipo Date");
+        dia = (function (d) { if (d < 9) return "0" + d; return "" + d; })(data.getDate());
+        mes = (function (m) { m += 1; if (m < 9) return "0" + m; return "" + m; })(data.getMonth());
+        ano = data.getFullYear();
         return dia + "/" + mes + "/" + ano;
     }
-    function _verificaRangeData(data) {
-        // "data" deve estar no formato dd/MM/yyyy
-        var arrayData = data.split("/").map(function (valor) { return parseInt(valor); });
-        if (arrayData[0] < 1 || arrayData[0] > 31) throw new Error("Data inválida");
-        if (arrayData[1] < 1 || arrayData[1] > 12) throw new Error("Data inválida");
-        if (arrayData[2] < 1901 || arrayData[2] > 4000) throw new Error("Data inválida");
+
+    function _desabilitar(selector, acao) {
+        var elements = _getElements(selector);
+        acao = acao | false;
+        function _action(e) {
+            if (acao) {
+                e.disabled = true;
+                _addClass(e, "ui-button-disabled ui-state-disabled");
+            } else {
+                e.disabled = false;
+                _removeClass(e, "ui-button-disabled ui-state-disabled");
+            }
+        }
+        _iterateElements(elements, _action);
+        return elements;
     }
 
-    if (!window.MaltaLib)
-        window.MaltaLib = {
-            formataData = _formataData
-        };
+    function _toogleElement(selector) {
+        var elements = _getElements(selector);
+        function _action(e) {
+            if (e.style.display == "none") {
+                e.style.display = "block";
+            } else { e.style.display = "none"; }
+        }
+        _iterateElements(elements, _action);
+        return elements;
+    }
+
+    function _isTypeof(type, obj) {
+        type = type.toLowerCase();
+        obj = obj.constructor.toString().toLowerCase();
+        return obj.indexOf(type) > -1;
+    }
+
+    function _getElements(selector) { return document.querySelectorAll(selector); }
+    function _addClass(element, className) { element.className += className; }
+    function _removeClass(element, classNames) {
+        var i, arrClassNames = classNames.split(" "), l = arrClassNames.length;
+        for (i = 0; i < l; i++) { element.className = element.className.replace(arrClassNames[i], ""); }
+        element.className = element.className.trim();
+    }
+    function _iterateElements(elements, callback) {
+        var i, l = elements.length;
+        for (i = 0; i < l; i++) { callback(element[i]); }
+    }
+
+    maltalib.formataData = _formataData;
+    maltalib.desabilitar = _desabilitar;
+    maltalib.toogle = _toogleElement;
+    maltalib.isTypeof = _isTypeof;
 })();
